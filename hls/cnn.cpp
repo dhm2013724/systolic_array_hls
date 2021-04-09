2ebc8183586dcac056907b7f0112fd927721d6e1
dhm2013724 <656314839@qq.com>
2021-04-09 13:58:08 +0800

    RTL-sim pass!

---
+++ hls/cnn.cpp
@@ -0,0 +1,171 @@
+
+#include "cnn.h"
+
+double what_time_is_it_now()
+{
+    struct timeval time;
+    if (gettimeofday(&time,NULL)){
+        return 0;
+    }
+    return (double)time.tv_sec + (double)time.tv_usec * .000001;
+}
+
+void copy_mem2dev(uint8_t *orig,uint32_t byte_num, unsigned long in_buffer)
+{
+	int fd = open("/dev/mem", O_RDWR);
+	unsigned char *virtual_addr;
+	uint32_t RequestByteNum;// must page
+	if(byte_num%(HPAGESIZE)==0)
+		RequestByteNum = byte_num;
+	else
+	{
+		RequestByteNum = ceil(byte_num/(HPAGESIZE*1.0))*(HPAGESIZE);
+	}
+	virtual_addr = (unsigned char *)mmap(NULL, RequestByteNum, PROT_READ | PROT_WRITE, MAP_SHARED, fd, (off_t)in_buffer);
+	if(virtual_addr == MAP_FAILED)
+	{
+		perror("Virtual_addr_in mappong for absolute memory access failed!\n");
+		return;
+	}
+	memcpy(virtual_addr,orig,byte_num);
+
+	munmap((void *)virtual_addr, byte_num);
+	close(fd);
+}
+
+void copy_dev2mem(uint8_t *dst,uint32_t byte_num, unsigned long in_buffer)
+{
+	int fd = open("/dev/mem", O_RDWR);
+	unsigned char *virtual_addr;
+	uint32_t RequestByteNum;// must page
+	if(byte_num%(HPAGESIZE)==0)
+		RequestByteNum = byte_num;
+	else
+	{
+		RequestByteNum = ceil(byte_num/(HPAGESIZE*1.0))*(HPAGESIZE);
+	}
+		virtual_addr = (unsigned char *)mmap(NULL, RequestByteNum, PROT_READ | PROT_WRITE, MAP_SHARED, fd, (off_t)in_buffer);
+	if(virtual_addr == MAP_FAILED)
+	{
+		perror("Virtual_addr_in mappong for absolute memory access failed!\n");
+		return;
+	}
+	memcpy((uint8_t *)dst,virtual_addr,byte_num);
+
+	munmap((void *)virtual_addr, byte_num);
+	close(fd);
+}
+
+int copy_file2mem(char *bin_file,uint32_t byte_num,unsigned long in_buffer)
+{
+	unsigned char *buffer = (unsigned char *)malloc(HPAGESIZE);
+	if(buffer==NULL){
+		printf("cannot malloc buffer %d byte\n", HPAGESIZE);
+		return -1;
+	}
+	printf("Total Byte Num = %d\n Address 0x%X\n", byte_num, in_buffer);
+	FILE *fp;
+	if( (fp = fopen(bin_file, "rb")) == NULL)fprintf(stderr,"CANNOT OPEN bin_file\n");
+	int rd_num;
+	unsigned long offset = 0;
+	while(rd_num = fread(buffer, sizeof(unsigned char), HPAGESIZE, fp))
+	{
+		if(rd_num < HPAGESIZE)
+			rd_num = HPAGESIZE;
+		copy_mem2dev(buffer,rd_num, in_buffer+offset);
+		printf("rd_num=%d, offset=%d\n", rd_num, offset);
+		offset += rd_num;
+	}
+	printf("copy_file2mem offset=%d\n",offset);
+	fclose(fp);
+
+	free(buffer);
+
+
+	return 0;
+}
+
+int copy_mem2file(char *bin_file,uint32_t byte_num,unsigned long in_buffer)
+{
+	void *buffer = malloc(HPAGESIZE);
+	if(buffer==NULL){
+		printf("cannot malloc buffer %d byte\n", HPAGESIZE);
+		return -1;
+	}
+
+	FILE *fp;
+	if( (fp = fopen(bin_file, "wb")) == NULL)fprintf(stderr,"CANNOT OPEN bin_file\n");
+
+	int x = byte_num;
+	int addbyte;
+	unsigned long offset = 0;
+	while(addbyte=((x<HPAGESIZE)?x:(HPAGESIZE)))
+	{
+		copy_dev2mem((uint8_t *)buffer,addbyte, in_buffer+offset);
+		fwrite(buffer , sizeof(unsigned char), addbyte, fp);
+		x -= addbyte;
+		offset += addbyte;
+	}
+	printf("copy_mem2file offset=%d\n",offset);
+
+
+	fclose(fp);
+
+	free(buffer);
+
+	return 0;
+}
+
+int MUL(int *A, int *B, int *C, int M, int N, int K)//A[MxK]*B[KxN]=C[MxN]
+{
+	int kloops = ceil(K*1.0/TILE_K);
+
+	copy_mem2dev((uint8_t *)A, M*K*4, AIN_BASEADDR);
+	copy_mem2dev((uint8_t *)B, K*N*4, BIN_BASEADDR);
+
+	unsigned int ap_idle;
+	unsigned int ap_done;
+
+	unsigned long int PhysicalAddress = ACC_BASEADDR;
+	int map_len = 0x80;
+	int fd = open("/dev/mem", O_RDWR);
+
+	unsigned char *xbase_address;
+	xbase_address = (unsigned char *)mmap(NULL, map_len, PROT_READ | PROT_WRITE, MAP_SHARED, fd, (off_t)PhysicalAddress);
+	if(xbase_address == MAP_FAILED)
+	{
+		perror("1:Init Mapping memory for absolute memory access failed.\n");
+		return -1;
+	}
+
+	while(1)
+	{
+		ap_idle = ((ReadReg(xbase_address, XMUL_CB_ADDR_AP_CTRL) >> 2) && 0x1);
+		if(ap_idle)
+			break;
+	}
+
+	WriteReg(xbase_address, XMUL_CB_ADDR_A_DATA,  AIN_BASEADDR);
+	WriteReg(xbase_address, XMUL_CB_ADDR_B_DATA,  BIN_BASEADDR);
+	WriteReg(xbase_address, XMUL_CB_ADDR_C_DATA,  COUT_BASEADDR);
+	WriteReg(xbase_address, XMUL_CB_ADDR_M_DATA,  M);
+	WriteReg(xbase_address, XMUL_CB_ADDR_N_DATA,  N);
+	WriteReg(xbase_address, XMUL_CB_ADDR_K_DATA,  K);
+	WriteReg(xbase_address, XMUL_CB_ADDR_KLOOPS_DATA,  kloops);
+
+	WriteReg(xbase_address, XMUL_CB_ADDR_GIE, 0x0);
+	WriteReg(xbase_address, XMUL_CB_ADDR_AP_CTRL, 0x1);//Start
+	while(1)
+	{
+		ap_done = ((ReadReg(xbase_address, XMUL_CB_ADDR_AP_CTRL) >> 1) && 0x1);
+		if(ap_done)
+			break;
+	}
+
+	munmap((void *)xbase_address, map_len);
+	close(fd);
+
+	copy_dev2mem((uint8_t *)C, M*N*4, COUT_BASEADDR);
+
+	return 0;
+}
--- hls/main.cpp
+++ hls/main.cpp
@@ -1,73 +1,85 @@
 
-#include <stdio.h>
-#include <stdlib.h>
-#include "systolic_array.h"
+#include "cnn.h"
 
-static int Array_A[NUM_M*NUM_K];
-static int Array_B[NUM_K*NUM_N];
-static int Array_C[NUM_M*NUM_N];
-//static int Array_C_Golden[NUM_M*NUM_N];
+//#define NUM_M 32
+//#define NUM_N 64
+//#define NUM_K 12
 
 int main(int argc, char *argv[]){
 
-	FILE *out_file;
-	out_file = fopen("output.dat","wb");
+	int NUM_M = 32;
+	int NUM_N = 64;
+	int NUM_K = 12;
+
+	if(argc>1)
+	{
+		NUM_M = atoi(argv[1]);
+		NUM_K = atoi(argv[2]);
+		NUM_N = atoi(argv[3]);
+	}
+
+	printf("M=%d, K=%d, N=%d\n", NUM_M, NUM_K, NUM_N);
+	int *Array_A        = (int *)malloc(NUM_M*NUM_K*sizeof(int));
+	int *Array_B        = (int *)malloc(NUM_K*NUM_N*sizeof(int));
+	int *Array_C        = (int *)malloc(NUM_M*NUM_N*sizeof(int));
+	int *Array_C_Golden = (int *)malloc(NUM_M*NUM_N*sizeof(int));
+	if((!Array_A)||(!Array_B)||(!Array_C)||(!Array_C_Golden)){
+		printf("alloc failed.\n");
+		return -1;
+	}
 
 	printf("TEST Systolic Array\n");
 	printf("=========START==========\n");
 
-//	for(int i=0; i<NUM_M; i++)
-//		for(int j=0; j<NUM_K; j++)
-//		{
-//			Array_A[i*NUM_K + j] = rand()%100;
-//		}
-//
-//	for(int i=0; i<NUM_K; i++)
-//		for(int j=0; j<NUM_N; j++)
-//		{
-//			Array_B[i*NUM_N + j] = rand()%100;
-//		}
-
 	for(int i=0; i<NUM_M; i++)
 		for(int j=0; j<NUM_K; j++)
 		{
-			Array_A[i*NUM_K + j] = (i*3+j*7)%100;
+			Array_A[i*NUM_K + j] = rand()%100;
 		}
 
 	for(int i=0; i<NUM_K; i++)
 		for(int j=0; j<NUM_N; j++)
 		{
-			Array_B[i*NUM_N + j] = (i*5+j*13)%100;
+			Array_B[i*NUM_N + j] = rand()%100;
 		}
 
-//	printf("sizeof(Array_C_Golden)=%d\n", sizeof(Array_C_Golden));
-//	memset(Array_C_Golden, 0, sizeof(Array_C_Golden));
-//
-//	for(int i=0; i<NUM_M; i++)
-//		for(int j=0; j<NUM_N; j++)
-//			for(int k=0; k<NUM_K; k++)
-//			{
-//				Array_C_Golden[i*NUM_N + j] += Array_A[i*NUM_K + k]*Array_B[k*NUM_N + j];
-//			}
+	memset(Array_C_Golden, 0, NUM_M*NUM_N*sizeof(int));
 
+	double cpu_time, fpga_time;
+	double start_t, end_t;
 
+	start_t = what_time_is_it_now();
+	for(int i=0; i<NUM_M; i++)
+		for(int j=0; j<NUM_N; j++)
+			for(int k=0; k<NUM_K; k++)
+			{
+				Array_C_Golden[i*NUM_N + j] += Array_A[i*NUM_K + k]*Array_B[k*NUM_N + j];
+			}
+	end_t = what_time_is_it_now();
+	cpu_time = end_t - start_t;
+	printf(" CPU Total time:%3.7lf\n", cpu_time);
+
+	start_t = what_time_is_it_now();
 	MUL( Array_A, Array_B, Array_C, NUM_M, NUM_N, NUM_K);
-
-//	int err = 0;
-//	for(int i=0; i<NUM_M*NUM_N; i++){
-//		if((Array_C_Golden[i]-Array_C[i])!=0){
-//			printf("[%d]:%d!=%d\n", i, Array_C[i], Array_C_Golden[i]);
-//			err++;
-//		}
-//	}
-
-	fwrite(Array_C,sizeof(int),NUM_M*NUM_N,out_file);
-	fclose(out_file);
+	end_t = what_time_is_it_now();
+	fpga_time = end_t - start_t;
+	printf("FPGA Total time:%3.7lf\n", fpga_time);
+	printf("FPGA/CPU speed = %3.7lf\n", cpu_time/fpga_time);
 
 	int err = 0;
-	err = system("diff --brief -w output.dat output.golden.dat");
-
+	for(int i=0; i<NUM_M*NUM_N; i++){
+		if((Array_C_Golden[i]-Array_C[i])!=0){
+			printf("[%d]:%d!=%d\n", i, Array_C[i], Array_C_Golden[i]);
+			err++;
+		}
+	}
 	printf("err_num = %d\n", err);
+
+	free(Array_A);
+	free(Array_B);
+	free(Array_C);
+	free(Array_C_Golden);
+
 	printf("==========END===========\n");
 	return err;
 }
